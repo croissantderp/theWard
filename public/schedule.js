bbc9538a0180dcfa574cc6d93e09e23c4ade8bdc
crested gecko <bookish.gecko@gmail.com>
2021-09-17 21:13:42 -0400

    about page and general functionality

--- public/schedule.js
+++ public/schedule.js
@@ -4,45 +4,53 @@ var fridayAs = [];
 var daysOff = [];
 var startDate = new Date("2021-9-9");
 var endDate = new Date("2022-6-24");
-var per1 = [new Date("1969", "1", "1"), new Date("1969", "1", "1")];
-var per2 = [new Date("1969", "1", "1"), new Date("1969", "1", "1")];
-var per3 = [new Date("1969", "1", "1"), new Date("1969", "1", "1")];
-var per4 = [new Date("1969", "1", "1"), new Date("1969", "1", "1")];
-var per5 = [new Date("1969", "1", "1"), new Date("1969", "1", "1")];
-var per6 = [new Date("1969", "1", "1"), new Date("1969", "1", "1")];
-var per7 = [new Date("1969", "1", "1"), new Date("1969", "1", "1")];
-var per8 = [new Date("1969", "1", "1"), new Date("1969", "1", "1")];
-var per9 = [new Date("1969", "1", "1"), new Date("1969", "1", "1")];
+var per = [[new Date("1969", "1", "1"), new Date("1969", "1", "1")],
+[new Date("1969", "1", "1"), new Date("1969", "1", "1")],
+[new Date("1969", "1", "1"), new Date("1969", "1", "1")],
+[new Date("1969", "1", "1"), new Date("1969", "1", "1")],
+[new Date("1969", "1", "1"), new Date("1969", "1", "1")],
+[new Date("1969", "1", "1"), new Date("1969", "1", "1")],
+[new Date("1969", "1", "1"), new Date("1969", "1", "1")],
+[new Date("1969", "1", "1"), new Date("1969", "1", "1")],
+[new Date("1969", "1", "1"), new Date("1969", "1", "1")]];
+
+var p = [];
 
 function initiate2() {
     let date = toUTC(new Date());
 
-    per1[0].setUTCHours(11, 5);
-    per1[1].setUTCHours(11, 45);
+    for (var i = 0; i < 9; i++) {
+        let temp = document.getElementById((i + 1).toString());
 
-    per2[0].setUTCHours(11, 50);
-    per2[1].setUTCHours(12, 30);
+        p.push(temp);
+    }
 
-    per3[0].setUTCHours(12, 35);
-    per3[1].setUTCHours(13, 15);
+    per[0][0].setUTCHours(11, 5);
+    per[0][1].setUTCHours(11, 45);
 
-    per4[0].setUTCHours(13, 20);
-    per4[1].setUTCHours(14, 0);
+    per[1][0].setUTCHours(11, 50);
+    per[1][1].setUTCHours(12, 30);
 
-    per5[0].setUTCHours(14, 5);
-    per5[1].setUTCHours(14, 45);
+    per[2][0].setUTCHours(12, 35);
+    per[2][1].setUTCHours(13, 15);
 
-    per6[0].setUTCHours(14, 50);
-    per6[1].setUTCHours(15, 30);
+    per[3][0].setUTCHours(13, 20);
+    per[3][1].setUTCHours(14, 0);
 
-    per7[0].setUTCHours(15, 35);
-    per7[1].setUTCHours(16, 15);
+    per[4][0].setUTCHours(14, 5);
+    per[4][1].setUTCHours(14, 45);
 
-    per8[0].setUTCHours(16, 20);
-    per8[1].setUTCHours(17, 00);
+    per[5][0].setUTCHours(14, 50);
+    per[5][1].setUTCHours(15, 30);
 
-    per9[0].setUTCHours(17, 05);
-    per9[1].setUTCHours(17, 45);
+    per[6][0].setUTCHours(15, 35);
+    per[6][1].setUTCHours(16, 15);
+
+    per[7][0].setUTCHours(16, 20);
+    per[7][1].setUTCHours(17, 00);
+
+    per[8][0].setUTCHours(17, 05);
+    per[8][1].setUTCHours(17, 45);
 
     if (date.getDay() == 1 || date.getDay() == 3) {
         setA();
@@ -106,75 +114,50 @@ function setA() {
 }
 
 function displayPeriod() {
-    let tempdate = toUTC(new Date());
+    let date = new Date();
+    let tempdate = toUTC(date);
 
     tempdate.setUTCFullYear("1969", "1", "1");
 
-    let p1 = document.getElementById("1");
-    let p2 = document.getElementById("2");
-    let p3 = document.getElementById("3");
-    let p4 = document.getElementById("4");
-    let p5 = document.getElementById("5");
-    let p6 = document.getElementById("6");
-    let p7 = document.getElementById("7");
-    let p8 = document.getElementById("8");
-    let p9 = document.getElementById("9");
+    var none = true;
+    var closestP;
+    var closest;
 
-    p1.classList.remove("highlight");
-    p2.classList.remove("highlight");
-    p3.classList.remove("highlight");
-    p4.classList.remove("highlight");
-    p5.classList.remove("highlight");
-    p6.classList.remove("highlight");
-    p7.classList.remove("highlight");
-    p8.classList.remove("highlight");
-    p9.classList.remove("highlight");
+    for (var i = 0; i < 9; i++) {
+        var temp = per[i][0] - tempdate;
+        if (temp > 0 && temp < closest) {
+            closestP = i;
+            closest = temp;
+        }
 
-    if (tempdate > per1[0] && tempdate < per1[1]) {
-        p1.children[1].innerHTML = "Ending in " + msToTime(per1[1] - tempdate);
-        p1.classList.remove("nothighlight");
-        p1.classList.add("highlight");
+        if (tempdate > per[i][0] && tempdate < per[i][1]) {
+            p[i].children[1].innerHTML = "<b>Ending in " + msToTime(per[i][1] - tempdate) + "</b>";
+            p[i].classList.remove("nothighlight");
+            p[i].classList.add("highlight");
+            none = false;
+        }
+        else {
+            if (p[i].classList.contains("highlight")) {
+                p[i].classList.remove("highlight");
+            }
+            if (!p[i].classList.contains("nothighlight")) {
+                p[i].classList.add("nothighlight");
+            }
+            if (p[i].children[1].innerHTML != "") {
+                p[i].children[1].innerHTML = "";
+            }
+        }
     }
-    if (tempdate > per2[0] && tempdate < per2[1]) {
-        p2.children[1].innerHTML = "Ending in " + msToTime(per2[1] - tempdate);
-        p2.classList.remove("nothighlight");
-        p2.classList.add("highlight");
+
+    if (tempdate > per[8][1]) {
+        p[8].children[1].innerHTML = "Ended " + msToTime(tempdate - per[8][1]) + " ago";
+        none = false;
     }
-    if (tempdate > per3[0] && tempdate < per3[1]) {
-        p3.children[1].innerHTML = "Ending in " + msToTime(per3[1] - tempdate);
-        p3.classList.remove("nothighlight");
-        p3.classList.add("highlight");
+
+    if (none) {
+        p[closestP].innerHTML = "Beginning in " + msToTime(closest);
     }
-    if (tempdate > per4[0] && tempdate < per4[1]) {
-        p4.children[1].innerHTML = "Ending in " + msToTime(per4[1] - tempdate);
-        p4.classList.remove("nothighlight");
-        p4.classList.add("highlight");
-    }
-    if (tempdate > per5[0] && tempdate < per5[1]) {
-        p5.children[1].innerHTML = "Ending in " + msToTime(per5[1] - tempdate);
-        p5.classList.remove("nothighlight");
-        p5.classList.add("highlight");
-    }
-    if (tempdate > per6[0] && tempdate < per6[1]) {
-        p6.children[1].innerHTML = "Ending in " + msToTime(per6[1] - tempdate);
-        p6.classList.remove("nothighlight");
-        p6.classList.add("highlight");
-    }
-    if (tempdate > per7[0] && tempdate < per7[1]) {
-        p7.children[1].innerHTML = "Ending in " + msToTime(per7[1] - tempdate);
-        p7.classList.remove("nothighlight");
-        p7.classList.add("highlight");
-    }
-    if (tempdate > per8[0] && tempdate < per8[1]) {
-        p8.children[1].innerHTML = "Ending in " + msToTime(per8[1] - tempdate);
-        p8.classList.remove("nothighlight");
-        p8.classList.add("highlight");
-    }
-    if (tempdate > per9[0] && tempdate < per9[1]) {
-        p9.children[1].innerHTML = "Ending in " + msToTime(per9[1] - tempdate);
-        p9.classList.remove("nothighlight");
-        p9.classList.add("highlight");
-    }
+
 }
 
 //from https://stackoverflow.com/questions/19700283/how-to-convert-time-in-milliseconds-to-hours-min-sec-format-in-javascript
